e84c721abd3f5db951a9e9fa1310e58efe8e3a63
valortin <victoronuohavictor@gmail.com>
2024-09-09 14:35:44 +0100

    smart contracts

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,24 +1,21 @@
-{
-  "$schema": "https://turbo.build/schema.json",
-  "globalDependencies": ["**/.env.*local"],
-  "tasks": {
-    "build": {
-      "dependsOn": ["^build"],
-      "outputs": [".next/**", "!.next/cache/**", "dist"]
-    },
-    "lint": {},
-    "dev": {
-      "cache": false,
-      "persistent": true
-    },
-    "test": {},
-    "start": {
-      "cache": false
-    },
-    "migrate": {
-      "cache": false
-    },
-    "prisma:generate": {},
-    "start:headless": {}
-  }
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
+  rules: {
+    'react/jsx-no-target-blank': 'off',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
 }
